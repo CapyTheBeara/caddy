9831402f5a7856c62e506c8b99cfcd2d0e0ac7b7
monocle <hchw29@gmail.com>
2014-08-26 07:13:46 -0700

    example of go test runner

---
+++ caddy.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"time"
+
+	"github.com/monocle/caddy/watcher"
+)
+
+var timeout = time.Second * 3
+
+func main() {
+	log.SetFlags(log.Lshortfile)
+
+	w := watcher.NewWatcher(&watcher.Config{
+		Dir:         ".",
+		IgnoreModes: []string{"chmod"},
+	})
+
+	for {
+		e := <-w.Events
+		w.IgnoreEvents = true
+
+		fmt.Print("\033[2J\033[0;0H")
+
+		cmd := exec.Command("go", "test")
+		cmd.Dir = filepath.Dir(e.Name)
+
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+
+		done := make(chan bool)
+
+		go func() {
+			err := cmd.Start()
+			if err != nil {
+				log.Println("[error]", err)
+			} else {
+				cmd.Wait()
+				close(done)
+			}
+		}()
+
+		select {
+		case <-done:
+		case <-time.After(timeout):
+			log.Println("[info] Unblocking long run")
+			if err := cmd.Process.Kill(); err != nil {
+				log.Fatal("[error]", err)
+			}
+
+		}
+
+		w.IgnoreEvents = false
+	}
+}
